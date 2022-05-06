7a7fb7c36f5250c453437cd8c891c16fc7448449
Caleb Lee <caleblee31101@gmail.com>
2022-05-05 20:40:04 -0500

    Added raspberry pi camera with more python threading

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from time import sleep, perf_counter
 from threading import Thread
+import cv2
 
 from Car import Car
 from Gps import Gps
 from Imu import Imu
+from webcamvideostream import WebcamVideoStream
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Response
 
 app = Flask(__name__)
 
 Car = Car(17,27)
 Gps = Gps()
 Imu = Imu()
+webcam = None
 
 roll = 0
 pitch = 0
@@ -49,6 +52,19 @@ def home():
     """
     return render_template("index.html")
 
+def gen(camera):
+    while True:
+        if camera.stopped:
+            break
+        frame = camera.read()
+        ret, jpeg = cv2.imencode('.jpg',frame)
+        if jpeg is not None:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+        else:
+            print("frame is none")
+        sleep(0.03)
+
 @app.route("/getGpsPosition", methods = ["GET"])
 def getGpsPosition():
     position = Gps.getPosition()
@@ -73,6 +89,13 @@ def getImuOrientation():
     )
     """
 
+@app.route('/video_feed')
+def video_feed():
+    global webcam
+    if webcam is None:
+        webcam = WebcamVideoStream()
+    return Response(gen(webcam.start()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threaded=True)
     
---
+++ webcamvideostream.py
@@ -0,0 +1,37 @@
+import cv2
+from threading import Thread
+import time
+import numpy as np
+
+class WebcamVideoStream:
+    def __init__(self, src = 0):
+        print("init")
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+        time.sleep(2.0)
+        self.t = None
+        print("init finished")
+        
+    def start(self):
+        if self.t is None:
+            print("start thread")
+            self.t = Thread(target=self.update, args=())
+            self.t.daemon = True
+            self.t.start()
+        return self
+    
+    def update(self):
+        print("read")
+        while True:
+            if self.stopped:
+                return
+            
+            (self.grabbed, self.frame) = self.stream.read()
+    
+    def read(self):
+        return self.frame
+    
+    def stop(self):
+        self.stopped = True
+
